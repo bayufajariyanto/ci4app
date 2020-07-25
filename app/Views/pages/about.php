1451e07830abc8e341a0c097ce2dee3e6222597e
bayufajariyanto <bayuf08@gmail.com>
2020-07-25 21:09:56 +0700

    init

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, dolorem quas. Aliquid ad eum quos, magni quia quis delectus quasi provident corrupti et repellendus, illum ex aliquam ratione doloribus ea eveniet exercitationem! Tenetur ducimus harum dolores quis nostrum repudiandae quaerat dolorum rem officiis sed culpa maxime autem, magni nobis ab possimus quo odio excepturi cupiditate blanditiis? Quam, fuga? Dolorem ad perspiciatis quo reiciendis necessitatibus nulla debitis pariatur enim optio quis. Voluptas alias accusamus dolorum at doloremque, pariatur sed vero sapiente, officia obcaecati quas excepturi voluptate laudantium minima tempore nam. Magnam dignissimos fugit sequi cum architecto iste alias placeat reiciendis voluptatum.</p>
+        </div>
+    </div>
+</div>
