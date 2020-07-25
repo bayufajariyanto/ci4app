8e2520908c29e3f0eeccef1910c4b3d4388475ef
bayufajariyanto <bayuf08@gmail.com>
2020-07-25 21:30:56 +0700

    navbar terpisah

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,16 +1,3 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <title><?= $title; ?></title>
-</head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">Bayu Fajariyanto</a>
@@ -32,5 +19,3 @@
         </div>
     </div>
 </nav>
-
-<body>
